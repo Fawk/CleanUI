eb36a4c25c517b7262bb3c70d7ac9fc39d1e053e
Fawk <oskar.k.karlsson@gmail.com>
2018-01-24 10:15:03 +0100

    Stuff

--- modules/config/profile.lua
+++ modules/config/profile.lua
@@ -4,6 +4,24 @@ local Profile, profiles = {}, {}
 local activeProfile = nil
 local currentCharacter = nil
 
+local function deepCopy(object)
+    local lookup_table = {}
+    local function _copy(object)
+        if type(object) ~= "table" then
+            return object
+        elseif lookup_table[object] then
+            return lookup_table[object]
+        end
+        local new_table = {}
+        lookup_table[object] = new_table
+        for index, value in pairs(object) do
+            new_table[_copy(index)] = _copy(value)
+        end
+        return setmetatable(new_table, getmetatable(object))
+    end
+    return _copy(object)
+end
+
 function Profile:Init(db)
 	for name, profile in pairs(db["Profiles"]) do
 		A:Debug("Profile:", name, "loaded!")
@@ -49,16 +67,38 @@ end
 function Profile:Copy(name)
 	local profile = profiles[name]
 	local newName = copyOfName(name)
-	A.db["Profiles"][newName] = A.db["Profiles"][profile]
+	
+	A.db["Profiles"][newName] = deepCopy(A.db["Profiles"][profile])
 	A.db["Characters"][currentCharacter] = newName
+
+	self:Init(A.db)
+	A.dbProvider:Save()
 end
 
-function Profile:Rename(name)
+function Profile:Rename(name, newName)
 	if activeProfile == name then
-		
+		A.db["Profiles"][newName] = deepCopy(A["Profile"])
 	else
-
+		A.db["Profiles"][newName] = deepCopy(profiles[name])
 	end
+
+	for char, profile in next, A.db["Characters"] do
+		if name == profile then
+			A.db["Characters"][char] = newName
+		end
+	end
+
+	self:Init(A.db)
+	self:SetActive(newName)
+	A.dbProvider:Save()
+end
+
+function Profile:Change(name)
+	A.db["Characters"][currentCharacter] = name
+
+	self:Init()
+	self:SetActive(name)
+	A.dbProvider:Save()
 end
 
 A["Modules"]["Profile"] = Profile
